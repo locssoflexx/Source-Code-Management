2767688e4ed0274892bd02b69d11937b0545f98e
locssoflexx <alexis.robinson3617@sinclair.edu>
2025-03-17 15:49:49 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Alexis Robinson
+// CIS 1202.501
+// March 17, 2025
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!\n";
+    return 0;
+}
+
